84b121a849e5e33a4b78803658f27747b26b1faa
shahzaibk23 <shahzaibceo@gmail.com>
2020-08-30 02:20:45 +0500

    Revert ".."
    This reverts commit 7f3113025e446ab1cc931f23661fe0b693664f03.

--- djangoProject2/user/views.py
+++ djangoProject2/user/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render, redirect
 from .forms import createUserForm, createUserForm2
 from django.contrib.auth import authenticate, login, logout
 from .models import Manager
-from pages.models import toDoListModel
-
 
 
 def RegisterView(request):
@@ -25,11 +23,6 @@ def RegisterView(request):
                     picture= form.cleaned_data.get('picture'),
                     user= form2.save()
                 )
-
-                toDoListModel.objects.create(
-                    manager = obj
-                )
-
 
 
 
